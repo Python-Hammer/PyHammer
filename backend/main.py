0863fddf82403efd8e8d4306024485928ae8b31f
Jérôme Viteau <jerome.viteau@pernod-ricard.com>
2025-07-28 16:44:29 +0200

    Move backend to dedicated folder

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,54 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
-from damage_calculator import calculate_damage
-from unit_data import get_unit_stats
+from rules.unit_profiles import *
+from metrics.unit_metric import *
+import numpy as np
+from data.loading import get_all_profiles, get_all_units
+from models.profile import Profile
 
-app = FastAPI()
+np.set_printoptions(precision=2, suppress=True)
 
-# Allow frontend to access backend
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-class DamageRequest(BaseModel):
-    attacker: str
-    defender: str
+def main():
+    ################ Load all units ################
 
-@app.post("/calculate-damage")
-def calculate_damage_endpoint(request: DamageRequest):
-    attacker_stats = get_unit_stats(request.attacker)
-    defender_stats = get_unit_stats(request.defender)
+    all_units = get_all_units()
+    knights = Profile(all_units["chaos_knights_charge"], is_reinforced=False)
+    dawnriders = Profile(all_units["scourge_of_ghyran_light_of_eltharion"])
 
-    if not attacker_stats or not defender_stats:
-        raise HTTPException(status_code=404, detail="Unit not found")
+    ################ Print a metric ################
 
-    damage = calculate_damage(attacker_stats, defender_stats)
-    return {"damage": damage}
+    metric = AlphaStrike(ennemy_unit=knights, scale_by_cost=False)
+    # metric = DPS(save=4, scale_by_cost=False)
+    # metric = AlphaStrike(ennemy_unit=knights, scale_by_cost=False)
+    # print(average_metric(units[1], metric, n_samples=10000))
+    multi_unit_plot_cdf([dawnriders, knights], metric, n_samples=10000)
+    # plot_cdf(dawnriders, metric, n_samples=10000)
+    """
+    ################ Plot different metrics ################
+     
+    metrics = [DPS(save=s, scale_by_cost=True) for s in [2, 3, 4, 5]]
+    multimetric_plot(units, metrics, n_samples=10000)
+
+    
+    ################ Plot different metrics ################
+     
+    metrics = [DPS(save=s, samples=100, scale_by_cost=True) for s in [2, 3, 4, 5]]
+   
+    multimetric_plot(units, metrics)
+    
+    ################ Tournament ################
+
+    metric = winrate(samples=100, initiative=1)
+
+    matrix(units, metric)
+
+
+    ################ Ranking according to a metric ################
+
+    metric = DPS(save=3, samples=100)
+
+    print(ranking(units, metric))
+    """
+
+
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -5,33 +5,41 @@
 Simulates a number of fights for a list of units then displays graphs with the results
 
 ### Quickstart
+
 Setup your virtual environment:
+
 ```shell
 python -m venv .venv
 ```
 
 then activate it:
+
 ```shell
 source .venv/bin/activate
 ```
 
 install the necessary packages 📦
+
 ```shell
 pip install -r requirements.txt
 ```
 
 run 🚀
+
 ```shell
 python main.py
 ```
 
 #### Setting up the backend
+
 Install FastAPI and uvicorn (should already be done if you installed requirements)
+
 ```shell
 pip install fastapi uvicorn
 ```
 
 Start the Development Server
+
 ```shell
 uvicorn main:app --reload
 ```
@@ -39,7 +47,9 @@ uvicorn main:app --reload
 This will start the FastAPI dev server at http://localhost:8000. You can also visit http://localhost:8000/docs to try the automatically generated API interactively.
 
 #### Setting up the frontend
+
 Install node and npm
+
 - Download and install from https://nodejs.org
 - Verify installation:
 
@@ -49,19 +59,20 @@ npm -v
 ```
 
 Install Dependencies
+
 ```shell
 cd frontend/
 npm install
 ```
 
 Start the Development Server
+
 ```shell
 cd frontend/
 npm run dev
 ```
 
 This will start the Vite dev server. Open your browser and navigate to http://localhost:5173 to view the app.
-
 
 ##### Project Structure
 
@@ -73,7 +84,6 @@ The project uses the standard Vite + React + TypeScript setup. Key folders:
 - `src/assets/` – Static assets (images, fonts, etc.)
 - `src/hooks/` – React hooks to handle state-based changes
 - `src/styles/` – css files
-
 
 ### Customizing
 
@@ -87,5 +97,5 @@ To add a unit to the simulation, insert it in the 'units' array in the **main.py
 - `"id": "crit_auto_wound"`: Flag the weapon as "Crit Auto Wound"
 - `"id": "crit_mortal"`: Flag the weapon as "Crit Mortal"
 - `"id": "crit_2_hits"`: Flag the weapon as "Crit 2 Hits"
-- `"id": "crit_5+"`: Flag the weapon as "Crit 5+" 
+- `"id": "crit_5+"`: Flag the weapon as "Crit 5+"
 - `"id": "add_X", "condition": Y, , "value": Z`: If condition Y (charged/...) is satified, add Z to value X (attacks, rend, hit, wound)
