1772a131bde49696d1772ec356a8bd56f89c17ee
Jérôme Viteau <jerome.viteau@pernod-ricard.com>
2025-07-29 10:36:06 +0200

    Adapt front for api unit select

--- frontend/src/components/unit_selector.tsx
+++ frontend/src/components/unit_selector.tsx
@@ -1,48 +1,64 @@
-import React from 'react';
-import DropdownMenu from './dropdown';
-import '../styles/unit_selector.css';
-import burgerIcon from '../assets/icon-burger.png';
-
+import React, { useEffect } from "react";
+import DropdownMenu from "./dropdown";
+import "../styles/unit_selector.css";
+import burgerIcon from "../assets/icon-burger.png";
 
 const UnitSelector = () => {
-  const units = [ "Steam Tank", "Immortis Guard" ];
-  const factions = [ "Order", "Death" ];
-  const weapons = [ "Sword", "Bow" ];
-  const [unit, setUnit] = React.useState('Steam Tank');
-  const [faction, setFaction] = React.useState('Order');
-  const [weapon, setWeapon] = React.useState('Sword');
+  const [units, setUnits] = React.useState<string[]>([]);
+  const [factions, setFactions] = React.useState<string[]>([]);
+  const weapons = ["Sword", "Bow"];
+  const [unit, setUnit] = React.useState("");
+  const [faction, setFaction] = React.useState("");
+
+  useEffect(() => {
+    const fetchFactions = async () => {
+      const response = await fetch("http://localhost:8000/faction_list");
+      const data = await response.json();
+      setFactions(data);
+    };
+    fetchFactions();
+  }, []);
+
+  useEffect(() => {
+    const fetchUnits = async () => {
+      if (faction) {
+        const response = await fetch(
+          `http://localhost:8000/faction_list/${faction}/units`
+        );
+        const data = await response.json();
+        setUnits(data);
+      } else {
+        setUnits([]);
+      }
+    };
+    fetchUnits();
+  }, [faction]);
 
   return (
     <div className="unit_selector">
-        <h2>{unit.toUpperCase()}</h2>
-        <DropdownMenu
-          label="Faction"
-          options={factions}
-          selection={faction}
-          onChange={setFaction}
+      <h2>{unit.toUpperCase()}</h2>
+      <DropdownMenu
+        label="Faction"
+        options={factions}
+        selection={faction}
+        onChange={setFaction}
+      />
+      <DropdownMenu
+        label="Unit"
+        options={units}
+        selection={unit}
+        onChange={setUnit}
+      />
+      <div className="unit_portrait">
+        <img
+          src={burgerIcon}
+          alt="Unit Portrait"
+          className="unit_portrait_img"
         />
-        <DropdownMenu
-          label="Unit"
-          options={units}
-          selection={unit}
-          onChange={setUnit}
-        />
-        <div className="unit_portrait">
-            <img
-                src={burgerIcon}
-                alt="Unit Portrait"
-                className="unit_portrait_img"
-            />
-            <p>{unit}</p>
-        </div>
-        <DropdownMenu
-          label="Weapon"
-          options={weapons}
-          selection={weapon}
-          onChange={setWeapon}
-        />
+        <p>{unit}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default UnitSelector;
